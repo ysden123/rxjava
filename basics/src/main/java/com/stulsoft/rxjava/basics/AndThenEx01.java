928c5b98066d36ae3851cbce7a0cf52bd37d69d8
Yuri Stul <yuriy.s@dauup-clicksmob.com>
2019-04-17 12:41:55 +0300

    AndThen

---
+++ basics/src/main/java/com/stulsoft/rxjava/basics/AndThenEx01.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2019. Yuriy Stul
+ */
+
+package com.stulsoft.rxjava.basics;
+
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+import io.reactivex.Single;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Yuriy Stul
+ */
+public class AndThenEx01 {
+    private static final Logger logger = LoggerFactory.getLogger(ChainWithError.class);
+
+    private void test1() {
+        logger.info("==>test1");
+        subscribe()
+                .subscribe(
+                        item -> logger.info("item: {}", item),
+                        error -> logger.error(error.getMessage())
+                );
+        logger.info("<==test1");
+    }
+
+    private void test2() {
+        logger.info("==>test2");
+        subscribe2()
+                .subscribe(
+                        item -> logger.info("item: {}", item),
+                        error -> logger.error(error.getMessage())
+                );
+        logger.info("<==test2");
+    }
+
+    Completable handshake() {
+        logger.info("==>handshake");
+        return Completable.create(observer -> {
+            logger.info("Making handshake");
+            observer.onComplete();
+        });
+    }
+
+    Flowable<String> subscription() {
+        logger.info("==>subscription");
+        return Flowable.fromArray("text1", "text2", "text3");
+    }
+
+    Flowable<String> subscribe() {
+        logger.info("==>subscribe");
+        return handshake().andThen(subscription());
+    }
+
+    Flowable<String> subscribe2() {
+        logger.info("==>subscribe2");
+        return Flowable.create(emitter -> {
+            handshake()
+                    .subscribe(() -> {
+                                subscription()
+                                        .subscribe(item -> emitter.onNext(item)
+                                                , error -> emitter.onError(error));
+                            },
+                            error -> emitter.onError(error));
+        }, BackpressureStrategy.BUFFER);
+
+    }
+
+    public static void main(String[] args) {
+        logger.info("==>main");
+        var c = new AndThenEx01();
+//        c.test1();
+        c.test2();
+        logger.info("<==main");
+    }
+}
